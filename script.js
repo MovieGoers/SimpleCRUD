c1ebab96808a2030b36e81a256c1402b977820d9
CodingFeature <121998353+MovieGoers@users.noreply.github.com>
2023-01-11 21:38:28 +0900

    CRUD 구현
    업데이트 및 열람 수정.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const lists = document.getElementById("lists");
+
 const text = document.getElementById("inputs");
+const textBig = document.getElementById("input-big");
+
 const btn = document.getElementById("btn");
-const nightdaybtn = document.getElementById("night-day");
-const mainBody = document.getElementById("main-body");
 const btndelete = document.getElementById("btndelete");
+const btnupdate = document.getElementById("btnupdate");
+
+const nightdaybtn = document.getElementById("night-day");
+
+const mainBody = document.getElementById("main-body");
+
 
 var i = 1;
+var listInside = [];
+var clickedListId = -1;
+
+listInside.push('temp');
 
 function onClickChangeNightDay(){
     if(mainBody.className === 'day'){
@@ -13,11 +24,19 @@ function onClickChangeNightDay(){
         document.body.style.color = "white";
         mainBody.className = "night";
     } else{
-        document.body.style.backgroundColor = "white";
+        document.body.style.backgroundColor = "cornflowerblue";
         document.body.style.color = "black";
         mainBody.className = "day";       
     }
     
+}
+
+function onClickList(event){
+    text.value=event.target.innerHTML;
+    textBig.value=listInside[event.target.id];
+
+    clickedListId = event.target.id;
+    document.getElementById('choice').innerHTML = 'You chose '+clickedListId+'.';
 }
 
 function onClick(){
@@ -27,10 +46,14 @@ function onClick(){
         let li = document.createElement('li');
     
         li.innerHTML = text.value;
+        listInside.push(textBig.value);
+
         li.id = i;
+        li.addEventListener('click', onClickList);
         lists.append(li);
     
         text.value='';
+        textBig.value='';
     
         i++;
     }
@@ -42,10 +65,24 @@ function onClickDelete(){
     }else{
         let lidelete = document.getElementById(i-1);
         lists.removeChild(lidelete);
+        listInside.pop();
         i--;
+    }
+}
+
+function onClickUpdate(){
+    if(clickedListId == Number(-1)){
+        alert('Please choose something on list to update!');
+    }else if(text.value===''){
+        alert('Please write something!');
+    }else{
+        templist = document.getElementById(clickedListId);
+        templist.innerHTML = text.value;
+        listInside[clickedListId] = textBig.value;
     }
 }
 
 nightdaybtn.addEventListener("click", onClickChangeNightDay);
 btn.addEventListener("click", onClick);
 btndelete.addEventListener("click", onClickDelete);
+btnupdate.addEventListener("click", onClickUpdate);
