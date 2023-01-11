1c19913b77f6080541e0bddfa58ae71b320238f4
CodingFeature <121998353+MovieGoers@users.noreply.github.com>
2023-01-11 18:12:04 +0900

    리스트 삭제 기능 구현

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const text = document.getElementById("inputs");
 const btn = document.getElementById("btn");
 const nightdaybtn = document.getElementById("night-day");
 const mainBody = document.getElementById("main-body");
+const btndelete = document.getElementById("btndelete");
+
+var i = 1;
 
 function onClickChangeNightDay(){
     if(mainBody.className === 'day'){
@@ -21,8 +24,18 @@ function onClick(){
     let li = document.createElement('li');
     
     li.innerHTML = text.value;
+    li.id = i;
     lists.append(li);
+
+    i++;
+}
+
+function onClickDelete(){
+    let lidelete = document.getElementById(i-1);
+    lists.removeChild(lidelete);
+    i--;
 }
 
 nightdaybtn.addEventListener("click", onClickChangeNightDay);
 btn.addEventListener("click", onClick);
+btndelete.addEventListener("click", onClickDelete);
