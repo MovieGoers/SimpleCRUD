d5b2f053d7ea89d7b5daa93c1f343344af7da172
CodingFeature <121998353+MovieGoers@users.noreply.github.com>
2023-01-11 16:55:58 +0900

    밤/낮 모드 추가.

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const title = document.getElementById("main-title");
 const text = document.getElementById("inputs");
 const btn = document.getElementById("btn");
+const nightdaybtn = document.getElementById("night-day");
+const mainBody = document.getElementById("main-body");
+
+function onClickChangeNightDay(){
+    if(mainBody.className === 'day'){
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        mainBody.className = "night";
+    } else{
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        mainBody.className = "day";       
+    }
+    
+}
 
 function onClick(){
     title.innerHTML = text.value;
 }
 
+nightdaybtn.addEventListener("click", onClickChangeNightDay);
 btn.addEventListener("click", onClick);
